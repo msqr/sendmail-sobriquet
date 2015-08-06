95608f731a084a3fb9182fef70e767a4d56bc72a
Matt Magoffin <git+matt@msqr.us>
2015-08-07 07:29:50 +1200

    Start wiring up base Java-based WebMVC configuration.

---
+++ sendmail-sobriquet/src/magoffin/matt/sorbirquet/web/HomeController.java
@@ -0,0 +1,46 @@
+/* ===================================================================
+ * HomeController.java
+ * 
+ * Created 7/08/2015 7:10:38 am
+ * 
+ * Copyright (c) 2015 Matt Magoffin.
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ * 02111-1307 USA
+ * ===================================================================
+ */
+
+package magoffin.matt.sorbirquet.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Controller for the app landing.
+ *
+ * @author matt
+ * @version 1.0
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+
+}
